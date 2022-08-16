6bc3fab4dd52d14bf2359c6ab7f6c79733cb229f
Carlos Diaz <92885752+Soyelchamps@users.noreply.github.com>
2022-08-15 21:06:39 -0500

    SongList Basico

---
+++ 03-react-context/src/componets/SongList.jsx/SongList.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+import './songlist.css'
+// Importar la lista de canciones de un JSON
+import canciones from '@/assets/listaCanciones.json'
+const SongList = () => {
+  const [list, setList] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  // simulamos llamada a la API
+  useEffect(() => {
+    setTimeout(() => {
+      setList(canciones)
+      setLoading(false)
+    }, 2000)
+  }, [])
+  return (
+    <section>
+      {loading
+        ? <h1>Cargando...</h1>
+        : list.map((song) => (
+          <div className='row-song' key={song.id}>
+            <h4>{song.title}</h4>
+            <p>{song.artist}</p>
+          </div>
+        ))}
+    </section>
+  )
+}
+
+export default SongList
--- 03-react-context/src/pages/Home/Home.jsx
+++ 03-react-context/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import SongList from '../../componets/SongList.jsx'
 import './home.css'
 const Home = () => {
   return (
     <div className='home-container'>
       <div className='izquierdo'>
         <h2>Lado Izquierdo</h2>
+        <SongList />
       </div>
       <div className='derecho'>
         <h2>Lado Derecho</h2>
